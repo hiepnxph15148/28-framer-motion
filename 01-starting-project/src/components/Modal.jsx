0c6be789d0f1d0ef22e853b47775c6b86ec8fd78
hiepnxph15148 <hiepnxph15148@fpt.edu.vn>
2025-04-03 19:17:55 +0700

    first commit

---
+++ 01-starting-project/src/components/Modal.jsx
@@ -0,0 +1,21 @@
+import { createPortal } from "react-dom";
+import { motion } from "framer-motion";
+export default function Modal({ title, children, onClose }) {
+  return createPortal(
+    <>
+      <div className="backdrop" onClick={onClose} />
+      <motion.dialog
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: -100 }}
+        transition={{ duration: 0.3 }}
+        open
+        className="modal"
+      >
+        <h2>{title}</h2>
+        {children}
+      </motion.dialog>
+    </>,
+    document.getElementById("modal")
+  );
+}
